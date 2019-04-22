b082cca7662540e180d685cd9d4e6751ffb49034
ahmed <ahmedabdo9864@yahoo.com>
2019-04-22 09:23:05 +0200

    deleted files reservation.php and pricesHotels.php

--- reservation.php
+++ reservation.php
@@ -6,13 +6,13 @@
 </head>
 <body>
 	<?php
-	echo'<div class = "contact-title">
+	cho'<div class = "contact-title">
 		<h1>Trivago</h1>
 		<h2>We are always ready to serve you!</h2>
 	</div>
 	<div class="contact-form">
 		<form id = "contact-form" method="post" action="hotel.php">
-			
+
 				<select class="contact-form" required name = "country">
 					<option value="" disabled selected>Country</option>
 					<option>Egypt</option>
@@ -20,16 +20,16 @@
 					<option>Canda</option>
 					<option>Alaska</option>
 		   		</select>
-		    
+
 			<br>
 			<input name="next" type="submit" class="price_reservation" value="Back" formaction = "index.php" formnovalidate>
 			<input name="next" type="submit" class="price_reservation" value="Next">
-		   	
-			
+
+
 		</form>
-		
+
 	</div>';
 	?>
-	
+
 </body>
 </html>
